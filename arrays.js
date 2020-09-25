8d5f751d49c8d3bd57ca6ff7a4c244896ca6b864
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-09-25 10:09:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,8 +31,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1)
-  return array
+  return array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
@@ -41,6 +40,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length - 1)
+  return array.slice(0, array.length - 1)
   return array
 }
